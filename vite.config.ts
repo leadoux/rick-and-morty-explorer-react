13fe734cbd9082fe0e9224c7ecb1eeb63a713daa
Adam Leadoux <adam@leadoux.com.au>
2026-04-03 10:00:00 +1100

    Initialize React + TypeScript app foundation with Vite tooling

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/graphql': {
+        target: 'https://rickandmortyapi.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
